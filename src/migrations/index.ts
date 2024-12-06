be4ff89d2f1d88b99ff6eea2ae0cdaf5ea2ec6b7
Shlomi Nugarker <shlomin1231@gmail.com>
2024-12-06 13:48:39 +0200

    migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241206_114009_migration_name from './20241206_114009_migration_name';
+
+export const migrations = [
+  {
+    up: migration_20241206_114009_migration_name.up,
+    down: migration_20241206_114009_migration_name.down,
+    name: '20241206_114009_migration_name'
+  },
+];
